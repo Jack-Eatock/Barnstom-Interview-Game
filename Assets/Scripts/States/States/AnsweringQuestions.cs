0bafa3d9452cd933a2fd9c8d815012a78b827e25
Jack Eatock <jack.eatock06@gmail.com>
2024-07-23 21:14:56 +0100

    Selecting Category

--- Assets/Scripts/States/States/AnsweringQuestions.cs
+++ Assets/Scripts/States/States/AnsweringQuestions.cs
@@ -6,10 +6,32 @@ namespace States
 {
 	public class AnsweringQuestions : BaseState
 	{
+		private IEnumerator awaitingSpinFinish;
+		WheelSection section;
+
 		public override void StateEnter()
 		{
 			base.StateEnter();
-			AudioManager.Instance.PlayClip("UiClick", 1f);
+			if (awaitingSpinFinish != null)
+				gameManager.StartCoroutine(awaitingSpinFinish);
+
+			awaitingSpinFinish = AwaitingSpinFinishes();
+			gameManager.StartCoroutine(AwaitingSpinFinishes());
+		}
+
+		private IEnumerator AwaitingSpinFinishes()
+		{
+
+
+			while (Mathf.Abs(gameManager.Wheel.Velocity) > .1f)
+				yield return new WaitForEndOfFrame();
+
+
+			Debug.Log("COMPLETE!");
+
+			// What category??
+			section = gameManager.Wheel.WhichWheelSectionDidWeLandOn();
+			Debug.Log("section " + section.Category.Text);
 		}
 	}
 }
